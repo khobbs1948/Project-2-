0416547ebc778a4b12e227212c2f16398101d92d
Naidu <Anisha.Naidu@solvay.com>
2022-06-09 23:05:11 -0400

    added database to sql

---
+++ mass_shootings.sql
@@ -0,0 +1,11 @@
+-- Create Table
+CREATE TABLE mass_shootings(
+index INT PRIMARY KEY,  
+year INT,
+state TEXT, 
+shootings INT,
+lawtotal INT,
+state_cumulative_shootings INT
+);
+
+select * from mass_shootings
