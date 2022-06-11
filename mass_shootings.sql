34f98c0a38df6fbec916b293d04fadbf4b9c093e
Naidu <Anisha.Naidu@solvay.com>
2022-06-11 11:08:25 -0400

    updated jupyter notebook to include a cleaned data table

--- mass_shootings.sql
+++ mass_shootings.sql
@@ -1,12 +1,24 @@
-drop table mass_shootings
+drop table mass_shootings_outer
+drop table mass_shootings_inner
+
 -- Create Table
-CREATE TABLE mass_shootings(
-index INT PRIMARY KEY,  
+CREATE TABLE mass_shootings_outer(
+index INT PRIMARY KEY,
 year INT,
 state TEXT, 
 shootings INT,
-lawtotal INT,
+number_gun_regulations INT,
 state_cumulative_shootings INT
 );
 
-select*from mass_shootings
+CREATE TABLE mass_shootings_inner(
+index INT PRIMARY KEY,
+year INT,
+state TEXT, 
+shootings INT,
+number_gun_regulations INT,
+state_cumulative_shootings INT
+);
+
+select*from mass_shootings_outer
+select*from mass_shootings_inner
